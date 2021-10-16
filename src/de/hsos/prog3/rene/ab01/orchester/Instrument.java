39b84cae6d05c5f2efe6fed160ce629bcf6379fd
rene <rene.beiermann@hs-osnabrueck.de>
2021-10-16 12:30:45 +0200

    hi

--- src/de/hsos/prog3/rene/ab01/orchester/Instrument.java
+++ src/de/hsos/prog3/rene/ab01/orchester/Instrument.java
@@ -1,15 +1,11 @@
 package de.hsos.prog3.rene.ab01.orchester;
 
-public enum Instrument { SAXOPHON, SCHLAGZEUG, AKKORDION;
+public enum Instrument { SAXOPHON("Baritone.wav"), SCHLAGZEUG("Drum.wav"), AKKORDION("Accordion.wav");
 
     private String audiodatei;
 
     Instrument(String audiodatei) {
         this.audiodatei = audiodatei;
-    }
-
-    Instrument() {
-
     }
 
     public String getAudiodatei() {
